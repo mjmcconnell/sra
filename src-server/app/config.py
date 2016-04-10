e2fda379b49a9012b14aafd12c6e17102e72eff8
Mark McConnell <mark@rehabstudio.com>
2016-04-10 23:35:29 +0100

    csp updated

--- src-server/app/config.py
+++ src-server/app/config.py
@@ -78,11 +78,11 @@ CONFIG = {
         'font-src': '\'self\' https://fonts.gstatic.com data ' +
                       'http://fonts.gstatic.com data:',
         'frame-src': '\'self\' https://www.youtube.com',
-        'script-src': '\'self\' ' +
+        'script-src': '\'self\' \'unsafe-eval\' ' +
                       'ajax.googleapis.com www.googletagmanager.com ' +
                       'www.google-analytics.com https://www.gstatic.com ' +
                       'https://www.googleadservices.com',
-        'style-src': '\'self\' ajax.googleapis.com ' +
+        'style-src': '\'self\' \'unsafe-inline\' ajax.googleapis.com ' +
                      'https://fonts.googleapis.com https://www.gstatic.com',
         'img-src': '\'self\' http://0.0.0.0:8080 *.googleusercontent.com www.google-analytics.com' +
                    ' https://www.gstatic.com data:',
