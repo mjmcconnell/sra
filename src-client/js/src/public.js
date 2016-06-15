d1ac9f2119eb7efe6944b150da9985761b79b685
Mark McConnell <mark@rehabstudio.com>
2016-06-15 08:14:41 +0100

    wip for hp workshop signup

--- src-client/js/src/public.js
+++ src-client/js/src/public.js
@@ -20,55 +20,88 @@ app.controller('NavCtrl', function($scope, $mdSidenav) {
 // Workshops controller
 app.controller('WorkshopsCtrl', function($scope, $mdDialog) {
 
-    $scope.openEventForm = function(event, index) {
-        $scope.activeEl = $scope.events[index];
+    $scope.showEventPrompt = function(event) {
         $mdDialog.show({
             controller: DialogController,
             template: `
-                <md-dialog id="workshopDialog" aria-label="Image Details" ng-cloak>
-                    <form name="workshopForm" novalidate class="workshop-form">
-                        <md-card>
-                            <md-card-content>
-                                <h1 class="md-subhead workshop-form-header-copy">Please fill in the form to sign up for the workshop</h1>
-                                <md-input-container class="md-block">
-                                    <label for="name">Name</label>
-                                    <input required id="name" name="name" type="text" ng-model="formData.name">
-                                    <div ng-messages="formData.name.$error" role="alert" class="md-input-message-animation">
-                                        <div ng-message="server" ng-repeat="error in serverErrors.name track by $index">{[error]}</div>
-                                    </div>
-                                </md-input-container>
-                                <md-input-container class="md-block">
-                                    <label for="email">Email Address</label>
-                                    <input required type="email" name="email" ng-model="formData.email" minlength="10" maxlength="100" ng-pattern="/^.+@.+\..+$/" />
-                                    <div ng-messages="formData.email.$error" role="alert" class="md-input-message-animation">
-                                        <div ng-message="server" ng-repeat="error in serverErrors.email track by $index">{[error]}</div>
-                                    </div>
-                                </md-input-container>
-                                <md-button class="contact-form-btn" ng-click="submitForm()">Sumbit</md-button>
-                            </md-card-content>
-                        </md-card>
-                    </form>
+                <md-dialog id="workshopDialog" ng-cloak>
+                    <md-toolbar>
+                      <div class="md-toolbar-tools">
+                            <h2>Sign up now</h2>
+                        </div>
+                    </md-toolbar>
+                    <md-dialog-content style="max-width:800px;max-height:810px; ">
+                        <form name="workshopForm" novalidate class="workshop-form" ng-submit="submitForm()">
+                            <md-input-container class="md-block">
+                                <label for="name">Name</label>
+                                <input required id="name" name="name" type="text" ng-model="formData.name">
+                                <div ng-messages="formData.name.$error" role="alert" class="md-input-message-animation">
+                                    <div ng-message="server" ng-repeat="error in serverErrors.name track by $index">{[error]}</div>
+                                </div>
+                            </md-input-container>
+                            <md-input-container class="md-block">
+                                <label for="email">Email Address</label>
+                                <input required type="email" name="email" ng-model="formData.email" minlength="10" maxlength="100" ng-pattern="/^.+@.+\..+$/" />
+                                <div ng-messages="formData.email.$error" role="alert" class="md-input-message-animation">
+                                    <div ng-message="server" ng-repeat="error in serverErrors.email track by $index">{[error]}</div>
+                                </div>
+                            </md-input-container>
+                            <md-button class="contact-form-btn" type="submit">Sumbit</md-button>
+                        </form>
+                    </md-dialog-content>
                 </md-dialog>`,
             parent: angular.element(document.body),
             scope: $scope,
             preserveScope: true,
             targetEvent: event,
             clickOutsideToClose:true
-        }).then(function(answer) {
-            // User chose an answer
-        }, function() {
-            // Dialog was closed
         });
     };
     function DialogController($scope, $mdDialog) {
+        $scope.formData = {};
+
         $scope.hide = function() {
             $mdDialog.hide();
         };
         $scope.cancel = function() {
             $mdDialog.cancel();
         };
-        $scope.answer = function(answer) {
-            $mdDialog.hide(answer);
+        $scope.submitForm = function(){
+            $scope.toastMessage = false;
+            $scope.serverErrors = {};
+            var uploadUrl = '/api/workshops/signup';
+
+            // Create an empty FormData object to store all form fields
+            var fd = new FormData();
+
+            // Loop through all other fields, and append them to the FormData object
+            for (var key in $scope.formData) {
+                if ($scope.formData[key]) {
+                    fd.append(key, $scope.formData[key]);
+                }
+            }
+
+            // Send the data of to the api, to be stored by the server
+            $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            })
+            .success(function(result){
+                $mdDialog.hide();
+                $scope.showSimpleToast('Message Sent');
+            }).error(function(error, status){
+                var error_message = 'Something went wrong, please check your form.'
+                if (error['message']) {
+                    error_message = error['message'];
+                } else if (error['error']) {
+                    error_message = error['error'];
+                }
+                for (var key in error['data']) {
+                    $scope.serverErrors[key] = error['data'][key];
+                }
+                $scope.showSimpleToast(error_message);
+            });
+        };
         };
     };
 });
