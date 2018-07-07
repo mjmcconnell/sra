a5f6f3b0d647f9addb4ad775747e6f16aa36a2ef
mark mcconnell <mark@whitespaceventures.com>
2018-07-07 16:47:39 +0100

    backend and frontend servers setup

--- src-server/appengine_config.py
+++
@@ -1,8 +0,0 @@
-"""`appengine_config` gets loaded when starting a new application instance.
-"""
-# stdlib imports
-import sys
-import os.path
-
-# add `third_party` subdirectory to `sys.path`, so we can load third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'third_party'))
