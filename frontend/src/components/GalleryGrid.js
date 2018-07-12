1e133c53bf98d15af458ba640ce8b85d09e08f26
mark mcconnell <mark@whitespaceventures.com>
2018-07-12 19:41:29 +0100

    seal added to gallery

--- frontend/src/components/GalleryGrid.js
+++ frontend/src/components/GalleryGrid.js
@@ -6,6 +6,8 @@ import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 
+import galleryData from '../data/galleryData';
+
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -14,39 +16,40 @@ const styles = theme => ({
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper
   },
-  galleryGrid: {
-    padding: '12px'
-  },
+  galleryGrid: {padding: '12px'},
   smTile: {},
   smTileImg: {maxWidth: '100%', maxHeight: '100%'},
   lrgTile: {},
-  lrgTileImg: {
-    maxWidth: '100%'
-  }
+  lrgTileImg: {width: '100%'}
 });
 
-const tileData = [
-  {
-    img: 'fox1.jpg',
-    tilte: 'fox1.jpg'
-  },
-  {
-    img: 'fox2.jpg',
-    tilte: 'fox2.jpg'
-  },
-  {
-    img: 'fox3.jpg',
-    tilte: 'fox3.jpg'
-  },
-  {
-    img: 'fox4.jpg',
-    tilte: 'fox4.jpg'
-  },
-  {
-    img: 'fox5.jpg',
-    tilte: 'fox5.jpg'
-  },
-];
+const gallerySmGrid = (classes, tileData) => {
+  return (
+    <Grid item sm={4} xs={12} className={classes.smTile}>
+      <GridList className={classes.gridList} cols={2} spacing={8}>
+        <GridListTile cols={2}>
+          <h1>{tileData.title}</h1>
+          <p>{tileData.desc}</p>
+        </GridListTile>
+        {tileData.images.map(image => (
+          <GridListTile key={image.src} cols={1}>
+            <img src={image.src} alt={image.title} className={classes.smTileImg}/>
+          </GridListTile>
+        ))}
+      </GridList>
+    </Grid>
+  );
+};
+
+const galleryLgGrid = (classes, tileData) => {
+  return (
+    <Hidden xsDown>
+      <Grid item sm={8} className={classes.lrgTile}>
+        <img src={tileData.images[0].src} alt={tileData.images[0].title} className={classes.lrgTileImg}/>
+      </Grid>
+    </Hidden>
+  );
+};
 
 class GalleryGrid extends React.Component {
   render() {
@@ -54,32 +57,13 @@ class GalleryGrid extends React.Component {
 
     return (
       <div className={classes.root}>
-        <Grid container spacing={24} className={classes.galleryGrid} spacing={8}>
-          <Grid item sm={4} xs={12} className={classes.smTile}>
-            <GridList className={classes.gridList} cols={2} spacing={8}>
-              <GridListTile cols={2}>
-                <h1>What's That fox</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-                  Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-                  Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-                  Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-                  Vestibulum lacinia arcu eget nulla.
-                </p>
-              </GridListTile>
-              {tileData.map(tile => (
-                <GridListTile key={tile.img} cols={1}>
-                  <img src={tile.img} alt={tile.title} className={classes.smTileImg}/>
-                </GridListTile>
-              ))}
-            </GridList>
+        {galleryData.map((tileData,i,a) => (
+          <Grid container spacing={24} className={classes.galleryGrid} spacing={8} key={tileData.title}>
+            {i % 2 === 0 && gallerySmGrid(classes, tileData)}
+            {galleryLgGrid(classes, tileData)}
+            {i % 2 === 1 && gallerySmGrid(classes, tileData)}
           </Grid>
-          <Hidden xsDown>
-            <Grid item sm={8} className={classes.lrgTile}>
-              <img src="fox3.jpg" alt="fox3.jpg" className={classes.lrgTileImg}/>
-            </Grid>
-          </Hidden>
-        </Grid>
+        ))}
       </div>
     );
   }
---
+++ frontend/src/data/galleryData.js
@@ -0,0 +1,66 @@
+const galleryData = [
+  {
+    title: 'What\'s That fox',
+    desc: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+        Nulla quis sem at nibh elementum imperdiet.
+    `,
+    images: [
+      {
+        src: 'fox1.jpg',
+        tilte: 'fox1.jpg'
+      },
+      {
+        src: 'fox2.jpg',
+        tilte: 'fox2.jpg'
+      },
+      {
+        src: 'fox3.jpg',
+        tilte: 'fox3.jpg'
+      },
+      {
+        src: 'fox4.jpg',
+        tilte: 'fox4.jpg'
+      },
+      {
+        src: 'fox5.jpg',
+        tilte: 'fox5.jpg'
+      },
+    ]
+  },
+  {
+    title: 'Seals',
+    desc: `
+        tummy up and tummy down seals
+    `,
+    images: [
+      {
+        src: 'seal1.jpg',
+        tilte: 'seal1.jpg'
+      },
+      {
+        src: 'seal2.jpg',
+        tilte: 'seal2.jpg'
+      },
+      {
+        src: 'seal3.jpg',
+        tilte: 'seal3.jpg'
+      },
+      {
+        src: 'seal4.jpg',
+        tilte: 'seal4.jpg'
+      },
+      {
+        src: 'seal5.jpg',
+        tilte: 'seal5.jpg'
+      },
+      {
+        src: 'seal6.jpg',
+        tilte: 'seal6.jpg'
+      },
+    ]
+  },
+];
+
+export default galleryData;
