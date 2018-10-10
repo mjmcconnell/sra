df1e60c254774f915d2629a9e9120cc38b5c8224
mark mcconnell <mark@whitespaceventures.com>
2018-10-10 20:12:53 +0100

    videos updated

--- frontend/src/components/WorkshopGrid.js
+++ frontend/src/components/WorkshopGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 
 import workshopData from '../data/workshopData';
 
@@ -16,21 +17,25 @@ const styles = theme => ({
   },
   title: {
     marginTop: 0
+  },
+  bannerImage: {
+    margin: '0 auto 10px'
   }
 });
 
 
-class Video extends React.Component {
-  iframe = (url) => {
+class BannerVideo extends React.Component {
+  iframe = (width, height) => {
     const htmlIframe = `
       <iframe
-        src=` + url + `
-        width="340"
-        height="260"
+        src=https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsharonreganart%2Fvideos%2F700281047017181%2F
+        width="` + width + `"
+        height="` + height + `"
         scrolling="no"
-        frameBorder="0"
+        frameborder="0"
         allowTransparency="true"
         allowFullScreen="true"
+        style="border:none;overflow:hidden"
       ></iframe>
     `
     return {
@@ -40,19 +45,49 @@ class Video extends React.Component {
 
   render() {
     return (
-      <div>
-        <div dangerouslySetInnerHTML={ this.iframe(this.props.url) } />
-      </div>
+      <div dangerouslySetInnerHTML={ this.iframe(this.props.width, this.props.height) } />
     );
   }
 };
 
+class Video extends React.Component {
+  iframe = (url) => {
+    const htmlIframe = `
+      <iframe
+        src=` + url + `
+        width="340"
+        height="260"
+        scrolling="no"
+        frameborder="0"
+        allowTransparency="true"
+        allowFullScreen="true"
+        style="border:none;overflow:hidden"
+      ></iframe>
+    `
+    return {
+      __html: htmlIframe
+    }
+  }
 
+  render() {
+    return (
+      <div dangerouslySetInnerHTML={ this.iframe(this.props.url) } />
+    );
+  }
+};
 function WorkshopGrid(props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
+      <div className={classes.bannerImage}>
+        <Hidden smDown>
+          <BannerVideo width={800} height={600}/>
+        </Hidden>
+        <Hidden mdUp>
+          <BannerVideo width={560} height={420}/>
+        </Hidden>
+      </div>
       <Grid container spacing={24}>
         {workshopData.map(tile => (
           <Grid item xs={12} sm={6} lg={4} key={tile.title}>
