914ce5ca41c68c1bdf7fcd6b089ce88a418a8aa5
Mark McConnell <mjmcconnell.dev@gmail.com>
2019-03-31 19:46:08 +0100

    new events added

--- frontend/src/data/eventData.js
+++ frontend/src/data/eventData.js
@@ -1,5 +1,58 @@
 const eventData = [
   {
+    title: 'Family Pet Portraits',
+    desc: [
+      `
+        Using air drying clay, Sharon will take the group step by step through
+        illustrated instructions, that will be provided to each pair.
+      `,
+      'This is a mother and child workshop, with the parent helping child or child helping parent.',
+      'Please bring photos of your pet\'s head, a hand towel and a small box to bring your sculpture home.',
+      `
+        Basic refreshments will be provided (juice, tea and coffee), but we ask you
+        to please bring a packed lunch with you, as it will be a long day.
+      `,
+    ],
+    image: {
+      src: '/images/events/class.jpg',
+      title: 'Family Pet Portraits'
+    },
+    date: '13th April 2019',
+    time: '10:00–15:00',
+    location: 'Project 24, Queens Parade, Bangor',
+    locationLink: 'https://www.google.com/maps/place/Project+24/@54.6619616,-5.6739291,15z/data=!4m2!3m1!1s0x0:0xd9841c736b09c6ef?ved=2ahUKEwiK6oDWjMzgAhVFonEKHQrzDDMQ_BIwCnoECAUQCA',
+    actionLabel: 'Facebook page',
+    actionLink: 'https://www.facebook.com/events/258063038411581/',
+    actionLinkTarget: '_blank'
+  },
+  {
+    title: 'Create Your Dragon',
+    desc: [
+      `
+        With the touring Game of Thrones Exhibition coming to the Titanic Exhibition Centre mid April,
+        Sharon will be hosting a workshop, taking a group step by step through illustrated instructions
+        to make their very own dragon, using air drying clay and a wire armature.
+      `,
+      'This is a mother and child workshop, with the parent helping child or child helping parent.',
+      'Please bring a hand towel and a small box to bring your sculpture home.',
+      `
+        Basic refreshments will be provided (juice, tea and coffee), but we ask you
+        to please bring a packed lunch with you, as it will be a long day.
+      `,
+    ],
+    image: {
+      src: '/images/events/dragon.jpg',
+      title: 'Art of the rails'
+    },
+    date: '4th May 2019',
+    time: '10:00–15:00',
+    location: 'Project 24, Queens Parade, Bangor',
+    locationLink: 'https://www.google.com/maps/place/Project+24/@54.6619616,-5.6739291,15z/data=!4m2!3m1!1s0x0:0xd9841c736b09c6ef?ved=2ahUKEwiK6oDWjMzgAhVFonEKHQrzDDMQ_BIwCnoECAUQCA',
+    actionLabel: 'Facebook page',
+    actionLink: 'https://www.facebook.com/events/409343713166086/',
+    actionLinkTarget: '_blank'
+  },
+  {
     title: 'Art on the Rails',
     desc: [
       'Applications now open for Art on the Rails',
@@ -12,7 +65,7 @@ const eventData = [
     ],
     image: {
       src: '/images/events/rails.jpg',
-      tilte: 'Art of the rails'
+      title: 'Art of the rails'
     },
     date: 'Last Sunday of the month, April to September',
     time: '11:00–15:00',
@@ -31,7 +84,7 @@ const eventData = [
       'Classes are currently available',
       'Sharon is currently holding workshop classes on Tuesday & Thursday evenings and Saturday mornings.',
       `
-        These workshops are designed to allow you to get stuck in creating your own master piece, with
+        These workshops are designed to allow you to get stuck in creating your own masterpiece, with
         the help and guidance for a well practiced artist.
       `,
       'Expect to get messy.',
@@ -39,7 +92,7 @@ const eventData = [
     ],
     image: {
       src: '/images/events/event2.jpg',
-      tilte: 'Open Art - Art on the Railings'
+      title: 'Open Art - Art on the Railings'
     },
     date: 'On Going',
     location: 'Project 24, Bangor',
@@ -55,7 +108,7 @@ const eventData = [
       'Sharon has posted a number of tutorial videos that you can follow along to, on her Facebook group.',
       `
         These tutorials are aimed at absolute beginner's and improver's, and comprises of
-        a series of video and photographs that break down the sculpting process into do-able stages.
+        a series of video and photographs that break down the sculpting process into doable stages.
         All the information is stored in units to make it easy to look things up, with links to
         recommended materials and tools.
       `,
@@ -64,7 +117,7 @@ const eventData = [
     ],
     image: {
       src: '/images/events/sra_workshop.jpg',
-      tilte: 'Online tutorials'
+      title: 'Online tutorials'
     },
     actionLabel: 'Join now',
     actionLink: 'https://m.facebook.com/groups/224194158189569',
